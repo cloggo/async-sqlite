7c3388a7cde9e3a8061c2b9ba97c51f35587a7a4
Aaron Diep <aaron@delite.ca>
2018-09-18 17:42:06 -0600

    bump up to 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.cloggo/async-sqlite "0.1.0"
+(defproject org.clojars.cloggo/async-sqlite "0.1.1"
   :description "orchestrate sqlite commands with core.async"
   :url "https://github.com/cloggo/async-sqlite"
   :license {:name "MIT" }
